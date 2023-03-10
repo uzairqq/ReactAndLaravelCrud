fb394e57ae0e8b34a17c75784f7bd954e56d8dc8
Uzair Iqbal <uzair.qq@outlook.com>
2023-03-11 04:33:45 +0500

    NAvbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
 
 function App() {
-  return <div className="container">Hello</div>;
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/"} className="nav-link">
+              Home
+            </Link>
+          </li>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
